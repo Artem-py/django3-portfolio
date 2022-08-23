900a875dfcc6f67c63c1440668da66870e91e602
Artem Chervontsev <artyom.chervontsev@gmail.com>
2022-08-23 19:08:59 +0300

    Second commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=500)
+    date = models.DateField()
+    description = models.CharField(max_length=100)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blogpost
+
+def all_blogs(request):
+    blogs = Blogpost.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
