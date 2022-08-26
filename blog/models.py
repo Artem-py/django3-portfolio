1aaf65085b2394b721a139464007f626f28b318c
Artem Chervontsev <artyom.chervontsev@gmail.com>
2022-08-26 13:57:06 +0300

    third commit

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blogpost(models.Model):
     text = models.TextField(max_length=500)
     date = models.DateField()
     description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blogpost
 
@@ -6,3 +6,6 @@ def all_blogs(request):
     blogs = Blogpost.objects.all()
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
+def detail(request, blog_id):
+    blog = get_object_or_404(Blogpost, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
